4552b0e7bae270edae0f64f0106e2acf8aae77b1
Tanish sharma <ts706893@gmail.com>
2024-05-09 13:19:28 +0530

    tanish

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -3,6 +3,43 @@ function App() {
   return (
    <h6> Chai aur react with vite | Tanish sharma
    React 
+   React 
+
+1 Why to learn React?
+- Hype,job,trend,build UI
+- Makes easy to manage & build complex frontend
+ 
+- React is a library 
+- framework vs library
+- framework - military
+- library - cooldude
+
+- Topics to learn-
+- Core of React(state or UI manipulation, JSX)
+- Component of Reusability
+- Reusing of Component (Props)
+- How to propagate change(hooks)
+
+- Additional addon to react
+- Router (React don't have Router)
+- State management (react don't have state management)
+-- redux,redux toolkit, zustand, context API
+- Class based component -- legacy code 
+-- BAAS(backend as a service) apps - social media clone , e commerce app
+
+- After react - React is not a complete solution in most case -- no seo, browser render of js , no routing
+-framework - next JS , Gatsby , Remix
+
+
+
+--- Can  i use react without a framework ???
+-- Yes , we can use vite or parcel bundler for that--- 
+-- what is a bundler??
+- bundler combines all the javascript files and provides us one basic file through which all the work is done
+
+
+- for mobile apps -- react + react native
+- for browser web related works --- react + react-dom
 
 </h6>
   
